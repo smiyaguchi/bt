19a439324b38d06f96edfd0ffcb8c21afc8f3d74
smiyaguchi <smiyaguchi96@gmail.com>
2021-07-19 00:32:15 +0900

    Parse cbt result data

--- cmd/bt/main.go
+++ cmd/bt/main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-    "github.com/smiyaguchi/bt/internal/parser"
+	"github.com/smiyaguchi/bt/internal/parser"
 )
 
 func main() {
-    fi, err := os.Stdin.Stat()
-    if err != nil {
-        panic(err)
-    }
-    if fi.Mode() & os.ModeNamedPipe != 0 {
-        _, err = parser.Parse(os.Stdin)
-        if err != nil {
-            panic(err)
-        }
-    }
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
+	if fi.Mode()&os.ModeNamedPipe != 0 {
+		t, err := parser.Parse(os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		for _, r := range t.Rows {
+			for k, v := range r.Value {
+				fmt.Printf("key: %s, value: %s\n", k, v)
+			}
+		}
+	}
 }
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,44 +1,95 @@
 package parser
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "os"
-    "time"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
-type Data struct {
-    Rows []Row
+const (
+	lineRowKey int = iota
+	lineFamily
+	lineValue
+)
+
+type Table struct {
+	Meta Meta
+	Rows []Row
+}
+
+type Meta struct {
+	Family []string
+	Column map[string]string // map[familyName]columnName
 }
 
 type Row struct {
-    Key string
-    ColumnFamilys []ColumnFamily
+	Key   string
+	Value map[string]string // map[family:identify@timestamp]value
 }
 
-type ColumnFamily struct {
-    Name string
-    Columns []Column
+func checkLine(text string) int {
+	if strings.HasPrefix(text, "    ") {
+		return lineValue
+	} else if strings.HasPrefix(text, "  ") {
+		return lineFamily
+	} else {
+		return lineRowKey
+	}
 }
 
-type Column struct {
-    Name string
-    Cells []Cell
+func parseRowKey(text string) Row {
+	return Row{
+		Key:   strings.TrimSpace(text),
+		Value: make(map[string]string, 0),
+	}
 }
 
-type Cell struct {
-    Value string
-    Timestamp time.Duration
+// TODO Add meta data
+func parseColumnKey(text string) string {
+	text = strings.TrimSpace(text)
+	return text
+	/*
+		splitIndex := strings.LastIndex(text, "@")
+		if splitIndex == -1 {
+			panic(fmt.Errorf("not found @"))
+		}
+		familyColumn := text[:splitIndex]
+		time := time.ParseDuration(text[splitIndex+1:])
+		family, column := strings.Split(familyColumn, ":")
+	*/
 }
 
-func Parse(r io.Reader) (*Data, error) {
-    scanner := bufio.NewScanner(os.Stdin)
-    for scanner.Scan() {
-        fmt.Println(scanner.Text())
-    }
-    if err := scanner.Err(); err != nil {
-        return nil, fmt.Errorf("error occured to scan: %v", err)
-    }
-    return nil, nil
+func parseValue(text string) string {
+	return strings.TrimSpace(text)
+}
+
+func Parse(r io.Reader) (*Table, error) {
+	line := strings.Repeat("-", 40)
+	table := Table{}
+	currentColumnKey := ""
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == line {
+			continue
+		}
+		switch checkLine(text) {
+		case lineRowKey:
+			table.Rows = append(table.Rows, parseRowKey(text))
+		case lineFamily:
+			currentColumnKey = parseColumnKey(text)
+			table.Rows[len(table.Rows)-1].Value[currentColumnKey] = ""
+		case lineValue:
+			table.Rows[len(table.Rows)-1].Value[currentColumnKey] = parseValue(text)
+		default:
+			panic(fmt.Errorf("no check pattern"))
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error occured to scan: %v", err)
+	}
+	return &table, nil
 }
